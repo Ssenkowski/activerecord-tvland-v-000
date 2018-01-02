25fe035a2ca652f396a00a7574444dd32e9dbfeb
Steven Senkowski <scsenkowski@gmail.com>
2018-01-02 13:50:11 +0000

    automatically backed up by learn

--- db/migrate/008_add_catchphrase_to_character.rb
+++ db/migrate/008_add_catchphrase_to_character.rb
@@ -1,5 +1,5 @@
 class AddCatchphaseToCharacter < ActiveRecord::Migration
   def change
-    add_column :characters, :actor_id, :integer
+    add_column :characters, :catchphrase, :string
   end
 end
